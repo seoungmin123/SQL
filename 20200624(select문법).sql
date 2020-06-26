c3cf36d6e45a6a6a21246c7db89bda62f158c153
lsm <sm1111176@gmail.com>
2020-06-26 11:22:10 +0900

    20200624 문법

---
+++ 20200624(select문법).sql
@@ -0,0 +1,26 @@
+--사용자 생성 및 테이블 생성
+user_create.sql
+scott.sql
+base_table.sql
+users.sql
+
+--SELECT 실습
+
+SELECT *
+FROM prod;
+
+SELECT prod_id, prod_name
+FROM prod;
+
+------0624 숙제----pt1번 55p
+SELECT *
+FROM lprod;
+
+SELECT buyer_id, buyer_name
+FROM buyer;
+
+SELECT *
+FROM cart;
+
+SELECT mem_id, mem_pass,mem_name
+FROM member;
